714ecc83102b641eb83de74bb43dfed15bd5d8b6
Paul Hartung <73059580+Smil3MoreGH@users.noreply.github.com>
2022-06-20 18:15:12 +0200

    # adding Register File
    Register File hinzugefügt - von Anton L.

---
+++ Register File/register_file_VHDL.cpp
@@ -0,0 +1,41 @@
+#include "register_file_VHDL.h"
+
+void register_file_VHDL::Behavioral()
+{
+	typedef <sc_lv<16>> reg_type[8];
+	sc_signal <reg_type> reg_array;
+	
+	if(rst='1')
+	{
+		reg_array(0).write(x"0001");
+		reg_array(1).write(x"0002");
+		reg_array(2).write(x"0003");
+		reg_array(3).write(x"0004");
+		reg_array(4).write(x"0005");
+		reg_array(5).write(x"0006");
+		reg_array(6).write(x"0007");
+		reg_array(7).write(x"0008");
+
+		else if(clk.event() && reg_write_rn == 1)
+		{
+			reg_array(to_int(sc_unit(reg_write_dest))) = reg_write_data;
+		}
+	}
+
+	if(reg_read_addr_1 = "000")
+	{
+		reg_read_data_1 = x"0000"
+	}
+	else 
+	{
+		reg_read_data_1 = reg_array(to_int(sc_unit(reg_read_addr_1)))
+	}
+	if(reg_read_addr_2 = "000")
+	{
+		reg_read_data_2 = x"0000"
+	}
+	else 
+	{
+		reg_read_data_2 = reg_array(to_int(sc_unit(reg_read_addr_2)))
+	}
+}
