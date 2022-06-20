edd72ff036e0baad6ed0c8d62f5116b4c0272751
Paul Hartung <73059580+Smil3MoreGH@users.noreply.github.com>
2022-06-20 22:04:16 +0200

    Update tb.cpp

--- Test Bench/tb.cpp
+++ Test Bench/tb.cpp
@@ -1,50 +1,48 @@
-//test bench
 #include "systemc.h"
-#include "" 77 ALLES FILES
+#include "alu_systemc.h"
+#include "alu_control_systemc.h"
+#include "control_unit_systemc.h"
+#include "data_memory_systemc.h"
+#include "instruction_memory_systemc.h"
+#include "MIPS_Processor.h"
+#include "register_file_systemc.h"
 #include "tb.h"
 
-SC_MODULE (MIPS_Processor)
+SC_MODULE (MIPS)
 {
-	tb *tb0;
-	//fir *fir0; //ersetzten mit unserem Namen
-	sc_clock clk_sig;
-	sc_signal<sc_lv<>> reset rst_sig;
-	sc_signal<sc_lv<16>>pc_out  pcout_sig;
-	sc_signal<sc_lv<16>>alu_result alur_sig;
-	
-	int sc_main(int argc, char* argv[])
-	{
-	void clk_process()
-	{
-	clk.write(0);
-	 wait( clk_period/2 , NS);
-	 clk.write(1);
-	 wait( clk_period/2 , NS);
-	}
+    sc_in <sc_lv> clk;
+    sc_in <sc_lv> reset;
+    sc_out <sc_lv<16>> pc_out;
+    sc_out <sc_lv<16>> alu_result;
 
-	void stim_proc()
-	{
-	reset.write(1);
-	wait( clk_period*10 , NS);
-	 reset.write(0);
-	 wait;
-	 
-	}
-	}
-	SC_CTOR (MIPS_Processor)
-	{
-	:clkp_sig("clkp_sig", 10, SC_NS);
-	tb0 = new tb("tb0");
-	tb0->clk(clk_sig);
-	tb0->reset(rst_sig);
-	tb0->pc_out(pcout_sig);
-	tb0->alu_result(alur_sig);
+    sc_signal <sc_lv> clk = 1;
+    sc_signal <sc_lv> clk = 1;
+    sc_signal <sc_lv<16>> pc_out;
+    sc_signal <sc_lv<16>> alu_result;
 
-	fir0 = new fir("fir0");
-	fir0->clk(clk_sig);
-	fir0->reset(rst_sig);
-	fir0->pc_out(pcout_sig);
-	fir0->alu_result(alur_sig);
-	}
+    const sc_time clk_period (10,SC_NS);
+    
+    MIPS_Processor MIPS("MIPS_name");
+    
+    MIPS.clk(clk);
+    MIPS.reset(reset);
+    MIPS.pc_out(pc_out);
+    MIPS.alu_result(alu_result);
 
+    // Clock process
+    void clk_process()
+    {
+        clk.write(0);
+        wait( clk_period/2 , NS);
+        clk.write(1);
+        wait( clk_period/2 , NS);
+    }
+    // Stimulus process
+    void stim_proc()
+    {
+        reset.write(1);
+        wait( clk_period*10 , NS);
+        reset.write(0);
+        wait;
+    }
 }
