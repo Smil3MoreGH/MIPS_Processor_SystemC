4b72d7d73ddc1059e74e085e64e05de825581c2f
Paul Hartung <73059580+Smil3MoreGH@users.noreply.github.com>
2022-06-20 19:55:02 +0200

    Update ALU_Control_Unit.h

--- ALU/ALU_Control_Unit.h
+++ ALU/ALU_Control_Unit.h
@@ -1,11 +1,13 @@
-SC_MODULE(ALU_Control_VHDL)
+#include "system.c"
+
+SC_MODULE(alu_control_systemc)
 {
     sc_out <sc_lv<3>> ALU_Control;
     sc_in <sc_lv<2>> ALUOp;
     sc_in <sc_lv<3>> ALU_Funct;
 
     
-    SC_CTOR(ALU_Control_VHDL)
+    SC_CTOR(alu_control_systemc)
     {      
         SC_METHOD(Behavioral);
         sensitive << ALUOp << ALU_Funct;
