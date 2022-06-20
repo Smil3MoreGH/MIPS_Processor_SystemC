04652f0a8df056362f5e0a66a8ae7d85410adf9d
Paul Hartung <73059580+Smil3MoreGH@users.noreply.github.com>
2022-06-20 21:20:40 +0200

    Update MIPS_Processor.h

--- MIPS Processor/MIPS_Processor.h
+++ MIPS Processor/MIPS_Processor.h
@@ -1,6 +1,6 @@
 #include "systemc.h"
 
-SC_MODULE(MIPS_VHDL)
+SC_MODULE(MIPS_Processor)
 {
     sc_in <sc_lv> clk;
     sc_in <sc_lv> reset;
@@ -9,7 +9,7 @@ SC_MODULE(MIPS_VHDL)
     sc_out <sc_lv<16>> alu_result;
     
     
-    SC_CTOR(MIPS_VHDL)
+    SC_CTOR(MIPS_Processor)
     {      
         SC_METHOD(Behavioral);
         sensitive << clk << reset;
