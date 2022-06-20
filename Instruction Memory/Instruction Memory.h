3910f7d1016a46a0dba25628e153211f53cf6152
Paul Hartung <73059580+Smil3MoreGH@users.noreply.github.com>
2022-06-20 20:05:15 +0200

    Update Instruction Memory.h

--- Instruction Memory/Instruction Memory.h
+++ Instruction Memory/Instruction Memory.h
@@ -1,12 +1,15 @@
 #include "systemc.h"
 
-SC_MODLUE(instruction_Memory_VHDL)
+SC_MODLUE(instruction_memory_systemc)
 {
 	sc_in<sc_bv<16>> pc;
 	sc_out<sc_bv<16>> instruction;
 
-	SC_CTOR(Instruction_Memory_VHDL)
+	SC_CTOR(instruction_memory_systemc)
 	{
 		SC_METHOD(Behavioral);
+		sensitivity << pc;
 	}
+	
+	void Behavioral();
 }
